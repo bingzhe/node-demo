7b4a5b604cd0c464d66578b26d00855f405f1e13
bingzhe <874177815@qq.com>
2018-06-19 19:13:13 +0800

    lesson7,8

---
+++ lesson8/app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+// 与之前一样
+var fibonacci = function (n) {
+    // typeof NaN === 'number' 是成立的，所以要判断 NaN
+    if (typeof n !== 'number' || isNaN(n)) {
+        throw new Error('n should be a Number');
+    }
+    if (n < 0) {
+        throw new Error('n should >= 0')
+    }
+    if (n > 10) {
+        throw new Error('n should <= 10');
+    }
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+};
+// END 与之前一样
+
+var app = express();
+
+app.get('/fib', function (req, res) {
+    var n = Number(req.query.n);
+
+    try {
+        res.send(String(fibonacci(n)));
+    } catch (e) {
+        res.status(500)
+            .send(e.message);
+    }
+
+});
+
+module.exports = app;
+
+app.listen(3000, function () {
+    console.log('app is listening at port 3000');
+});
